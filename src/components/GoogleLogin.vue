861415ff8d74134e2fc368a07589b2260f60ed27
Ayaz Rachmadiev <a.rachmadiev@student.han.nl>
2023-04-19 12:27:08 +0200

    Styling klaar

---
+++ src/components/GoogleLogin.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import { googleAuthCodeLogin } from "vue3-google-login"
+const login = () => {
+  googleAuthCodeLogin().then((response) => {
+    console.log("Handle the response", response)
+  })
+}
+</script>
+
+<template>
+  <button @click="login">Login Using Google</button>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import vue3GoogleLogin from 'vue3-google-login'
 import './assets/main.css'
+
 
 const app = createApp(App)
 
 app.use(router)
 
+app.use(vue3GoogleLogin, {
+    clientId: '803841872814-pv42lrb233jb0ct4pcksr3t464gcdu0f.apps.googleusercontent.com'
+})
+
 app.mount('#app')
+
+
+
+
+
+
+
+
+
