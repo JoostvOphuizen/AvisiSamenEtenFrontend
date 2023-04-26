eefb0613905129ae7c309dd671acaf01c6bdbefe
Joost Ophuizen, van <93910542+JoostvOphuizen@users.noreply.github.com>
2023-04-26 10:53:55 +0200

    fixt schaalbaarheid bij login pagina

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -27,7 +27,7 @@ export default ({
   <div class="center">
     <GlassTile class="glass" >
       <h1 class="titletext">Login</h1>
-      <AppButton @click="login" label="Login with Google" iconLeft="src\assets\Google-modern-flat-icon.svg"></AppButton>
+      <AppButton @click="login" label="Login with Google" iconLeft="src\assets\Google-modern-flat-icon.svg" class="button"></AppButton>
     </GlassTile>
   </div>
 </template>
@@ -35,13 +35,13 @@ export default ({
 <style scoped>
 
 .glass {
-  margin: 0 auto;
+  margin: 0;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   padding: 40px;
-  width: 40%;
+  min-width: 200px;
 }
 
 .center{
@@ -56,6 +56,21 @@ export default ({
   font-weight: 500;
   color: var(--vt-c-white);
   margin: 0 0 20px 0;
+}
+
+.button {
+  margin: 20;
+  width: 100%;
+  min-width: 180px;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
+/* media queries */
+@media screen and (max-width: 350px) {
+  .glass {
+    width: 100%;
+  }
 }
 
 </style>
