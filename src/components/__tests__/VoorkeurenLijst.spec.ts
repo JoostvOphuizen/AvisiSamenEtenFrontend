a90a57157385eba4c2f68bc246b44fc6179c1b4a
Ayaz Rachmadiev <a.rachmadiev@student.han.nl>
2023-04-26 11:03:50 +0200

    Jenkins update

--- src/components/__tests__/VoorkeurenLijst.spec.ts
+++ src/components/__tests__/VoorkeurenLijst.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import VoorkeurenLijst from '../VoorkeurenLijst.vue'
 
-describe('HelloWorld', () => {
+describe('Voorkeuren', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(VoorkeurenLijst, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
