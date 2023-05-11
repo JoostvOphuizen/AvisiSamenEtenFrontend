55b1d72cbc8d8ae7fcc5e087d203eb24806b8823
Joost Ophuizen, van <93910542+JoostvOphuizen@users.noreply.github.com>
2023-05-11 11:15:42 +0200

    updates the directories and files that where rewritten

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
-import VoorkeurView from '../views/VoorkeurViewCopy.vue'
+import VoorkeurView from '../views/VoorkeurView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/VoorkeurView.vue
+++ src/views/VoorkeurView.vue
@@ -1,49 +1,137 @@
-<script lang="ts">
-import VoorkeurenLijst from '@/components/VoorkeurenLijst.vue'
-import { mapActions, mapGetters } from 'vuex'
-import { defineComponent, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
-import SearchBar from '@/components/SearchBar.vue'
-
-export default {
-  data() {
-    return {
-      searchQuery: '',
-    }
-  },
-  components: {
-    VoorkeurenLijst,
-    SearchBar
-  },
-  computed: {
-    ...mapGetters(['isLoggedIn']),
-  },
-  mounted() {
-    if (!this.isLoggedIn) {
-      this.$router.push('/login')
-    }
-  },
-  methods: {
-    performSearch(searchTerm: string) {
-      this.searchQuery = searchTerm
-    }
-  }
-}
-</script>
-
 <template>
   <div class="categorybox">
-    <!-- <SearchBar @search="performSearch" /> -->
-    <VoorkeurenLijst v-if="isLoggedIn" />
+    <CheckboxList :items="voorkeurCheckboxItems" @update:items="handleCheckboxItemsUpdate" />
+    <CheckboxList :items="voedingrestrictieCheckboxItems" @update:items="handleVoedingrestrictieCheckboxItemsUpdate" />
+    <AppButton label="Opslaan" @click="postUserVoorkeuren"></AppButton>
   </div>
 </template>
 
-<style scoped>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CheckboxList from '@/components/CheckboxList.vue';
+import { get, post } from '@/services/apiService';
+import { mapGetters } from 'vuex'
+import AppButton from '@/components/Button.vue';
 
+const baseURL = "http://localhost:8080";
+
+interface CheckboxItem {
+  label: string;
+  value: boolean;
+}
+
+export default defineComponent({
+  components: {
+    CheckboxList,
+    AppButton,
+  },
+
+  data() {
+    return {
+      voorkeurCheckboxItems: [] as CheckboxItem[],
+      voedingrestrictieCheckboxItems: [] as CheckboxItem[],
+    };
+  },
+
+  computed: {
+    checkedItems(): CheckboxItem[] {
+      return this.voorkeurCheckboxItems.filter((item: CheckboxItem) => item.value);
+    },
+    ...mapGetters(['isLoggedIn', 'getUserID']),
+  },
+
+  methods: {
+    handleCheckboxItemsUpdate(updatedItems: CheckboxItem[]) {
+      this.voorkeurCheckboxItems = updatedItems;
+    },
+
+    handleVoedingrestrictieCheckboxItemsUpdate(updatedItems: CheckboxItem[]) {
+      this.voedingrestrictieCheckboxItems = updatedItems;
+    },
+
+    async fetchAllVoorkeuren() {
+      try {
+        const data = await get(`${baseURL}/voorkeuren`);
+        this.voorkeurCheckboxItems = data.voorkeuren.map((item: any) => ({
+          label: item.naam,
+          value: false,
+        }));
+      } catch (error) {
+        // Handle error
+        console.log(error);
+      }
+    },
+
+    async fetchUserVoorkeuren(){
+      try {
+        const data = await get(`${baseURL}/gebruiker/haalvoorkeurenop?id=`+this.getUserID);
+        for (let i = 0; i < this.voorkeurCheckboxItems.length; i++) {
+          for (let j = 0; j < data.voorkeuren.length; j++) {
+            if (this.voorkeurCheckboxItems[i].label == data.voorkeuren[j].naam) {
+              this.voorkeurCheckboxItems[i].value = true;
+            }
+          }
+        }
+      } catch (error) {
+        // Handle error
+        console.log(error);
+      }
+    },
+
+    async postUserVoorkeuren(){
+      try {
+        var checkedItemsWithoutValue = [];
+        for (let i = 0; i < this.checkedItems.length; i++) {
+          checkedItemsWithoutValue.push(this.checkedItems[i].label);
+        }
+        const postData = {
+          voorkeuren: checkedItemsWithoutValue
+        }
+        const data = await post(`${baseURL}/gebruiker/slavoorkeurenop?id=${this.getUserID}`, postData);
+      } catch (error) {
+        // Handle error
+        console.log(error);
+      }
+    },
+
+    async mockGetAllRestricties(){
+      this.voedingrestrictieCheckboxItems = [
+        { label: 'Gluten', value: false },
+        { label: 'Lactose', value: false },
+        { label: 'Noten', value: false },
+        { label: 'Pinda', value: false },
+        { label: 'Sesam', value: false },
+      ];
+    },
+    async mockGetUserRestricties(){
+      var userVoedingrestrictie = [
+        { label: 'Gluten', value: true },
+        { label: 'Noten', value: true },
+      ];
+
+      for (let i = 0; i < this.voedingrestrictieCheckboxItems.length; i++) {
+        for (let j = 0; j < userVoedingrestrictie.length; j++) {
+          if (this.voedingrestrictieCheckboxItems[i].label == userVoedingrestrictie[j].label) {
+            this.voedingrestrictieCheckboxItems[i].value = true;
+          }
+        }
+      }
+    },
+  },
+
+  async mounted() {
+    await this.fetchAllVoorkeuren();
+    this.fetchUserVoorkeuren();
+    await this.mockGetAllRestricties();
+    this.mockGetUserRestricties();
+  },
+});
+</script>
+
+<style scoped>
 /* fills screen */
 .categorybox {
   width: 100%;
   height: 100%;
 }
-
 </style>
