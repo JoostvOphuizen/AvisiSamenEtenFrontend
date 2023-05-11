9bda8bf69facf84ed7380584eec75897f936073b
Joost Ophuizen, van <93910542+JoostvOphuizen@users.noreply.github.com>
2023-05-11 11:14:22 +0200

    voegt front-end voor voedingsRestricties toe

--- src/views/VoorkeurViewCopy.vue
+++ src/views/VoorkeurViewCopy.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="categorybox">
-    <CheckboxList :items="checkboxItems" @update:items="handleCheckboxItemsUpdate" />
-    <div>
-      Updated items:
-      <ul>
-        <li v-for="(item, index) in checkedItems" :key="index">{{ item.label }}</li>
-      </ul>
-    </div>
+    <CheckboxList :items="voorkeurCheckboxItems" @update:items="handleCheckboxItemsUpdate" />
+    <CheckboxList :items="voedingrestrictieCheckboxItems" @update:items="handleVoedingrestrictieCheckboxItemsUpdate" />
+    <AppButton label="Opslaan" @click="postUserVoorkeuren"></AppButton>
   </div>
-  <AppButton label="Opslaan" @click="postUserVoorkeuren"></AppButton>
 </template>
 
 <script lang="ts">
@@ -30,25 +25,34 @@ export default defineComponent({
     CheckboxList,
     AppButton,
   },
+
   data() {
     return {
-      checkboxItems: [] as CheckboxItem[], // Empty array to be populated with fetched data
-    }
+      voorkeurCheckboxItems: [] as CheckboxItem[],
+      voedingrestrictieCheckboxItems: [] as CheckboxItem[],
+    };
   },
+
   computed: {
     checkedItems(): CheckboxItem[] {
-      return this.checkboxItems.filter((item: CheckboxItem) => item.value);
+      return this.voorkeurCheckboxItems.filter((item: CheckboxItem) => item.value);
     },
     ...mapGetters(['isLoggedIn', 'getUserID']),
   },
+
   methods: {
-    handleCheckboxItemsUpdate(updatedItems: CheckboxItem[]) { 
-      this.checkboxItems = updatedItems;
+    handleCheckboxItemsUpdate(updatedItems: CheckboxItem[]) {
+      this.voorkeurCheckboxItems = updatedItems;
     },
+
+    handleVoedingrestrictieCheckboxItemsUpdate(updatedItems: CheckboxItem[]) {
+      this.voedingrestrictieCheckboxItems = updatedItems;
+    },
+
     async fetchAllVoorkeuren() {
       try {
         const data = await get(`${baseURL}/voorkeuren`);
-        this.checkboxItems = data.voorkeuren.map((item: any) => ({
+        this.voorkeurCheckboxItems = data.voorkeuren.map((item: any) => ({
           label: item.naam,
           value: false,
         }));
@@ -57,13 +61,14 @@ export default defineComponent({
         console.log(error);
       }
     },
+
     async fetchUserVoorkeuren(){
       try {
         const data = await get(`${baseURL}/gebruiker/haalvoorkeurenop?id=`+this.getUserID);
-        for (let i = 0; i < this.checkboxItems.length; i++) {
+        for (let i = 0; i < this.voorkeurCheckboxItems.length; i++) {
           for (let j = 0; j < data.voorkeuren.length; j++) {
-            if (this.checkboxItems[i].label == data.voorkeuren[j].naam) {
-              this.checkboxItems[i].value = true;
+            if (this.voorkeurCheckboxItems[i].label == data.voorkeuren[j].naam) {
+              this.voorkeurCheckboxItems[i].value = true;
             }
           }
         }
@@ -72,6 +77,7 @@ export default defineComponent({
         console.log(error);
       }
     },
+
     async postUserVoorkeuren(){
       try {
         var checkedItemsWithoutValue = [];
@@ -82,16 +88,42 @@ export default defineComponent({
           voorkeuren: checkedItemsWithoutValue
         }
         const data = await post(`${baseURL}/gebruiker/slavoorkeurenop?id=${this.getUserID}`, postData);
-        console.log(data);
       } catch (error) {
         // Handle error
         console.log(error);
       }
-    }
+    },
+
+    async mockGetAllRestricties(){
+      this.voedingrestrictieCheckboxItems = [
+        { label: 'Gluten', value: false },
+        { label: 'Lactose', value: false },
+        { label: 'Noten', value: false },
+        { label: 'Pinda', value: false },
+        { label: 'Sesam', value: false },
+      ];
+    },
+    async mockGetUserRestricties(){
+      var userVoedingrestrictie = [
+        { label: 'Gluten', value: true },
+        { label: 'Noten', value: true },
+      ];
+
+      for (let i = 0; i < this.voedingrestrictieCheckboxItems.length; i++) {
+        for (let j = 0; j < userVoedingrestrictie.length; j++) {
+          if (this.voedingrestrictieCheckboxItems[i].label == userVoedingrestrictie[j].label) {
+            this.voedingrestrictieCheckboxItems[i].value = true;
+          }
+        }
+      }
+    },
   },
+
   async mounted() {
     await this.fetchAllVoorkeuren();
     this.fetchUserVoorkeuren();
+    await this.mockGetAllRestricties();
+    this.mockGetUserRestricties();
   },
 });
 </script>
