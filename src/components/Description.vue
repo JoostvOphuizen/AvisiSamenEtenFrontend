e4f52a80654251b0f9fabc8d387ed8256d627b16
Rieke Jansen <R.Jansen29@student.han.nl>
2023-05-12 15:05:18 +0200

    restaurant pagina

---
+++ src/components/Description.vue
@@ -0,0 +1,80 @@
+<script lang="ts">
+import GlassTile from "@/components/GlassTile.vue";
+
+export default {
+  name: 'Description',
+  components: {GlassTile},
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    telefoonnummer: {
+      type: String,
+      required: false
+    },
+    image: {
+      type: String,
+      required: false
+    },
+    adres: {
+      type: String,
+      required: false
+    }
+  },
+}
+</script>
+<style scoped>
+.Flexbox {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 60%;
+  overflow-x: auto;
+}
+.row {
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 10px;
+  align-items: flex-end;
+}
+.leftItem {
+  flex-grow: 3;
+  text-align: left;
+}
+.rightItem {
+  flex-grow: 3;
+  text-align: right;
+}
+.plaatje {
+  display: flex;
+  flex-direction: row;
+  flex-basis: 100%;
+  flex-wrap: wrap;
+  align-items: stretch;
+}
+.picto {
+  width: 25px;
+  filter: invert(1);
+}
+</style>
+<template>
+  <div class="Flexbox">
+  <h1>{{title}}</h1>
+  <GlassTile class="row">
+    <div class="row">
+      <img class="plaatje" alt="restaurant" :src="this.image"/>
+      <span class="leftItem">
+        <img class="picto" alt="restaurant" src="src/assets/telephone.png"/>
+        {{telefoonnummer}}
+      </span>
+      <span class="rightItem">
+        {{adres}}
+        <img class="picto" alt="restaurant" src="src/assets/location.png"/>
+      </span>
+    </div>
+  </GlassTile>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import VoorkeurView from '../views/VoorkeurView.vue'
+import RestaurantView from "@/views/RestaurantView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,7 +24,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: VoorkeurView
-    }
+    },
+    {
+      path: '/restaurant',
+      name: 'restaurant',
+      component: RestaurantView
+    },
   ]
 })
 
