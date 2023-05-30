08f4dd9228e139715af61e2da4a6c12533ab09c5
B-HANgit <BAH.vanderVen@student.han.nl>
2023-05-30 10:40:05 +0200

    opbouw review pagina volledig

--- src/views/ReviewView.vue
+++ src/views/ReviewView.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="center">
-    <!-- <Description
-      hierbinnen moet de restaurant data komen
-    ></Description> -->
-    <span class="title">
-      <h1 class="h1text">Review</h1>
-      <h1 class="blueText">.</h1>
-    </span>
-    <GlassTile class="glass">
-      <Rating @waarde="getWaardeRating"></Rating>
-      <Inputveld @search="getWaardeInput" :placeholder-name="'Geef een toelichting...'"></Inputveld>
-    </GlassTile>
-    <AppButton label="Verstuur" @click="verstuurReview"></AppButton>
+  <div class="display">
+    <div class="center">
+      <Description
+        :title="'Placeholder Restaurant'"
+        :image="'https://cdn.discordapp.com/attachments/1096361791287738490/1108039901561233428/latest.png'"
+        :adres="'1234AB' + ' ' + '5' + ' ' + 'straatnaam'"
+        :link="'https://confluenceoosevt.aimsites.nl/display/XUSYOU/Home'"
+      ></Description>
+    </div>
+    <div class="center">
+      <span class="title">
+        <h1 class="h1text">Review</h1>
+        <h1 class="blueText">.</h1>
+      </span>
+      <GlassTile class="glass">
+        <Rating @waarde="getWaardeRating"></Rating>
+        <Inputveld @search="getWaardeInput" :placeholder-name="'Geef een toelichting...'"></Inputveld>
+      </GlassTile>
+      <AppButton label="Verstuur" @click="verstuurReview"></AppButton>
+    </div>
   </div>
 </template>
 
@@ -72,6 +79,14 @@ export default {
 
 
 <style scoped>
+.display {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  overflow-x: auto;
+}
+
 .h1text {
     color: var(--vt-c-white);
     margin: 0;
@@ -95,16 +110,6 @@ export default {
     justify-content: space-between;
 }
 
-
-.flexbox {
-  display: flex;
-  flex-direction: column;
-  align-items:  baseline;
-  width: 60%;
-  width: fit-content;
-  overflow-x: auto;
-}
-
 .center {
   display: flex;
   justify-content: center;
@@ -114,7 +119,22 @@ export default {
 }
 
 .title {
-  margin: 5px 50px 30px 50px;
+  margin: 5px 30px 10px 30px;
+}
+
+@media screen and (max-width: 400px) {
+  .glass {
+    width: 100%;
+    overflow-x: auto;
+  }
+
+}
+
+@media screen and (max-width: 800px) {
+  .display {
+    flex-direction: column;
+  }
+
 }
 
 </style>
