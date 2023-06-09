b41d9b2577b441eee02240d02e1392c98998c5cc
JoostvOphuizen <93910542+JoostvOphuizen@users.noreply.github.com>
2023-06-09 12:45:16 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import './assets/main.css'
 
 
 createApp(App)
+                            /* Add an key from Google Cloud Platform */
     .use(vue3GoogleLogin, {clientId: ''})
     .use(router)
     .use(store)
